1dfb4ee84d081dd2102af7f6af8c1b9c8f0de22b
Baraa Kolly <baraa.ali.kolly@gmail.com>
2020-05-16 16:58:39 +0200

    finish week2 project

--- homework/index.js
+++ homework/index.js
@@ -1,24 +1,26 @@
 'use strict';
 
 {
-  const REPOS_UL_CLASS_NAME = 'repos-ul';
-  const LI_CLASS_NAME = 'li-class'
-  const UL_TABLE_CLASS_NAME = 'ul-table';
   const ALERT_ERROR = 'alert-error';
-  const HEAD_CLASS_NAME = 'head';
+
   function fetchJSON(url, cb) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.responseType = 'json';
-    xhr.onload = () => {
-      if (xhr.status >= 200 && xhr.status <= 299) {
-        cb(null, xhr.response);
-      } else {
-        cb(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
-      }
-    };
-    xhr.onerror = () => cb(new Error('Network request failed'));
-    xhr.send();
+    const root = document.getElementById('root');
+    const loadingDiv = createAndAppend('div', root, { text: 'Loading', class: 'loading' })
+    fetch(url)
+      .then(resopnse => {
+        if (!resopnse.ok)
+          cb(new Error(`HTTP error status : ${resopnse.status}`));
+        else
+          return resopnse.json();
+      })
+      .then(data => {
+        loadingDiv.innerText = '';
+        if (data)
+          cb(null, data);
+      })
+      .catch(error => {
+        cb(new Error('There has been a problem with your fetch operation:', error));
+      })
   }
 
   function createAndAppend(name, parent, options = {}) {
@@ -34,13 +36,13 @@
     return elem;
   }
 
-  function createTable(repo,tableClassName) {
+  function createTable(repo, tableClassName) {
     const table = document.createElement('table');
-    table.setAttribute('class' , tableClassName);
+    table.setAttribute('class', tableClassName);
     const tableInfo = `<tbody>
     <tr>
         <td>Repository:</td>
-        <td><a href = "">${repo.name}</a></td>
+        <td><a href = ${repo.html_url} target = "_blank"> ${repo.name}</a></td>
     </tr>
     <tr>
         <td>Description:</td>
@@ -55,20 +57,32 @@
         <td>${new Date(repo.updated_at).toLocaleDateString()}, ${new Date(repo.updated_at).toLocaleTimeString()}</td>
     </tr>
   </tbody>`
-  table.innerHTML = tableInfo;
-  return table;
-  }
-  function renderRepoDetails(repo, ul) {
-    createAndAppend('li', ul , { class : LI_CLASS_NAME});
-    const liItem = document.querySelector('li:last-child');
-    liItem.appendChild(createTable(repo , UL_TABLE_CLASS_NAME ) );
+    table.innerHTML = tableInfo;
+    return table;
   }
 
-  function main(url,numberOfRepos) {
-    fetchJSON(`${url}?per_page=${numberOfRepos}`, (err, repos) => {
-      const root = document.getElementById('root');
-      const head = createAndAppend('div', root, { class : HEAD_CLASS_NAME })
-      head.innerText = 'HYF Repositories';
+  function renderRepoDetails(repo, ul) {
+    ul.innerHTML = ''
+    createAndAppend('li', ul, { class: 'li-class' });
+    const liItem = document.querySelector('li:last-child');
+    liItem.appendChild(createTable(repo, 'ul-table'));
+  }
+
+  function renderContributorDetailes(contribtor) {
+    const main = document.getElementById('contributor-main');
+    const row = createAndAppend('div', main, { class: 'row' })
+    const columnPic = createAndAppend('div', row, { class: 'column-pic' });
+    const pic = createAndAppend('img', columnPic, { class: 'avatar-pic' })
+    pic.setAttribute('src', `${contribtor.avatar_url}`);
+    const columnContent = createAndAppend('div', row, { class: 'column-content' });
+    const content = createAndAppend('a', columnContent, { text: `${contribtor.login}`, href: `${contribtor.html_url}` });
+    content.setAttribute('target', '_blank');
+    const columnNum = createAndAppend('div', row, { text: `${contribtor.contributions}`, class: 'column-num' });
+  }
+
+  function addContributores(repo) {
+    const CONTR_URL = repo.contributors_url;
+    fetchJSON(CONTR_URL, (err, contribtores) => {
       if (err) {
         createAndAppend('div', root, {
           text: err.message,
@@ -76,15 +90,67 @@
         });
         return;
       }
-      const ul = createAndAppend('ul', root, { class : REPOS_UL_CLASS_NAME});
-      const sortedRepos = repos.sort(function(a, b) {
+      const main = document.getElementById('contributor-main');
+      main.innerHTML = '';
+      contribtores.forEach(contributor => {
+        renderContributorDetailes(contributor);
+      });
+
+    })
+  }
+
+  function main(url, numberOfRepos) {
+    fetchJSON(`${url}?per_page=${numberOfRepos}`, (err, repos) => {
+      const root = document.getElementById('root');
+      if (err) {
+        createAndAppend('div', root, {
+          text: err.message,
+          class: ALERT_ERROR,
+        });
+        return;
+      }
+      const repoSection = document.querySelector('.repo-container');
+      const header = document.querySelector('.header');
+      const headDiv = createAndAppend('div', header, { class: 'head' })
+      const selectRepo = createAndAppend('select', header, { class: 'select-repo' })
+      const ul = createAndAppend('ul', repoSection, { class: 'repos-ul' });
+      headDiv.innerText = 'HYF Repositories';
+
+      const sortedRepos = repos.sort(function (a, b) {
         return a.name.localeCompare(b.name);
       });
-      sortedRepos.forEach(repo => renderRepoDetails(repo, ul));
+      sortedRepos.forEach((repo, index) => {
+        createAndAppend('option', selectRepo, {
+          text: repo.name,
+          value: index
+        });
+      });
+      renderRepoDetails(sortedRepos[0], ul);
+      addContributores(sortedRepos[0]);
+      selectRepo.addEventListener('change', () => {
+        fetchJSON(`${url}?per_page=${numberOfRepos}`, (err, repos) => {
+          if (err) {
+            createAndAppend('div', root, {
+              text: err.message,
+              class: ALERT_ERROR,
+            });
+            return;
+          }
+          const sortedRepos = repos.sort(function (a, b) {
+            return a.name.localeCompare(b.name);
+          });
+          const selectedRepo = selectRepo.options[selectRepo.selectedIndex].value;
+          renderRepoDetails(sortedRepos[selectedRepo], ul)
+          addContributores(sortedRepos[selectedRepo]);
+        })
+      })
     });
+
   }
 
   const HYF_REPOS_URL =
     'https://api.github.com/orgs/HackYourFuture/repos';
-  window.onload = () => main(HYF_REPOS_URL,10);
+  window.onload = () => main(HYF_REPOS_URL, 10);
+
 }
+
